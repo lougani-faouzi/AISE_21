d68147254baef146c905bfe32321aefd5a507dab
lougani-faouzi <faouzi.lougani@hotmail.com>
2021-03-06 15:44:02 +0100

    add print_help method

--- IHM.h
+++ IHM.h
@@ -2,8 +2,11 @@
 #define _IHM_h_
 
 #include "processlist.h"
+
 void print_processlist(processlist_info *p);
 void print_Uptime(int val_sensor);
 void print_Load_Average(double val1,double val2,double val3);
 void print_memory_result(unsigned long long int,unsigned long long int,unsigned long long int,unsigned long long int);
+void print_help(int argc, char **argv);
+
 #endif
